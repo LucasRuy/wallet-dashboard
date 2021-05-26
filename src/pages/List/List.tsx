04c2cf8546a62d95fda34f812e94a42ed1b346db
Lucas Ruy <lukas.r@hotmail.com>
2021-05-25 23:45:44 -0300

    feat: create a initial folder structure of pages directory

---
+++ src/pages/List/List.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const List: React.FC = () => {
+  return (
+    <h1>List Screen</h1>
+  )
+}
+
+export default List
---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignIn: React.FC = () => {
+  return (
+    <h1>SignIn Screen</h1>
+  )
+}
+
+export default SignIn
