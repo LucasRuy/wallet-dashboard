432ef5101e2fd0cbf0bb25976b2ff39212f570a3
Lucas Ruy <lukas.r@hotmail.com>
2021-05-27 00:48:40 -0300

    feat: create Content component

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as s from './styles'
+
+const Content: React.FC = () => {
+  return (
+    <s.Container>
+      <p>Content</p>
+    </s.Container>
+  )
+}
+
+export default Content
