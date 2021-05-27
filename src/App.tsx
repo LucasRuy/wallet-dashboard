0cda1c7c3569ae2aa8ad1ea29ec7a4fee956fb80
Lucas Ruy <lukas.r@hotmail.com>
2021-05-27 01:53:26 -0300

    feat: set ThemeProvider on the app root

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from './styles/GlobalStyles'
+import darkTheme from './styles/themes/dark'
 
 import Dashboard from './pages/Dashboard'
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
       <Dashboard />
-    </>
+    </ThemeProvider>
   )
 }
 
