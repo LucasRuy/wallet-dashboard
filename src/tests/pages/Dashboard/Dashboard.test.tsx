ddadbd995209cbc00c743700d97ce2091490f77f
Lucas Ruy <lukas.r@hotmail.com>
2021-05-27 02:02:33 -0300

    fix: adjust broken test of dashboard component

--- src/tests/pages/Dashboard/Dashboard.test.tsx
+++ src/tests/pages/Dashboard/Dashboard.test.tsx
@@ -1,10 +1,17 @@
 import { render, queryByTestId } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+
+import darkTheme from '../../../styles/themes/dark'
 
 import Dashboard from '../../../pages/Dashboard'
 
 describe('<Dashboard />', () => {
   test('Should be render "Dashboard Screen" phrase', () => {
-    render(<Dashboard />)
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <Dashboard />
+      </ThemeProvider>
+    )
 
     const dashboard = queryByTestId(document.documentElement, 'dashboard')
     expect(dashboard).toBeInTheDocument()
