84297ad1e78944410b775ec6ac49eb1ef958dce4
Lucas Ruy <lukas.r@hotmail.com>
2021-05-27 00:48:28 -0300

    feat: create Aside component

---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as s from './styles'
+
+const Aside: React.FC = () => {
+  return (
+    <s.Container>
+      <p>Aside</p>
+    </s.Container>
+  )
+}
+
+export default Aside
---
+++ src/components/Aside/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  grid-area: AS;
+  background-color: #003459;
+  color: #FFFFFF;
+`
