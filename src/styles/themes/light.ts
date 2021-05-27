00a6fd8a245e200c1d163d7fe04a8f150d9d5ff6
Lucas Ruy <lukas.r@hotmail.com>
2021-05-27 01:52:24 -0300

    feat: create lightTheme object

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,19 @@
+const lightTheme = {
+  title: 'light',
+  colors: {
+    primary: '#99E6FF',
+    secondary: '#6FCCF1',
+    tertiary: '#99B6FF',
+
+    gray: '#8D99AE',
+    white: '#141414',
+    black: '#FFFFFF',
+
+    success: '#99D98C',
+    warning: '#F4A261',
+    error: '#E5383B',
+    info: '#2087F4'
+  }
+}
+
+export default lightTheme
