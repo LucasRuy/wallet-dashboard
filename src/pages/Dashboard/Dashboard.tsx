da83a6fc0c1705b73a3b6c780e59bc310a0e0a83
Lucas Ruy <lukas.r@hotmail.com>
2021-05-27 00:48:00 -0300

    feat: create Layout component with grid template

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import Layout from '../../components/Layout'
+
 const Dashboard: React.FC = () => {
   return (
-    <h1>Dashboard Screen</h1>
+    <Layout />
   )
 }
 
