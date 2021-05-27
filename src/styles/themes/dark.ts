6b7579570a707232282f220688c73f3e801867de
Lucas Ruy <lukas.r@hotmail.com>
2021-05-27 01:52:09 -0300

    feat: create darkTheme object

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,19 @@
+const darkTheme = {
+  title: 'dark',
+  colors: {
+    primary: '#00171F',
+    secondary: '#003459',
+    tertiary: '#007EA7',
+
+    gray: '#8D99AE',
+    white: '#FFFFFF',
+    black: '#212529',
+
+    success: '#99D98C',
+    warning: '#F4A261',
+    error: '#E5383B',
+    info: '#4EA8dE'
+  }
+}
+
+export default darkTheme
