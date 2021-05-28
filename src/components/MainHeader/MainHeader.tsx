a962de53a7e2e736e404bacf2d18bc69de7ae9e6
Lucas Ruy <lukas.r@hotmail.com>
2021-05-28 01:19:59 -0300

    feat: create Profile component with random emojis inside

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import * as s from './styles'
 
+import emojis from '../../utils/emojis'
+
 const MainHeader: React.FC = () => {
+  const getEmoji = useMemo(() => {
+    return emojis[Math.floor(Math.random() * emojis.length)];
+  }, [])
+
   return (
     <s.Container>
-      <p>MainHeader</p>
+      <p>Toogle</p>
+      <s.Profile>
+        <h3>Hello, {getEmoji}</h3>
+        <span>User Name</span>
+      </s.Profile>
     </s.Container>
   )
 }
