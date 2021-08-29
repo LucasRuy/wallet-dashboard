8cc75fa895b59be930b65c5781a79469ff6571d2
Lucas Ruy <lukas.r@hotmail.com>
2021-08-29 13:25:43 -0300

    feat(content-header): create base of ContentHeader component

---
+++ src/components/ContentHeader/ContentHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as s from './styles'
+
+const ContentHeader: React.FC = () => {
+  return (
+    <s.Container>
+      <p>ContentHeader</p>
+    </s.Container>
+  )
+}
+
+export default ContentHeader
